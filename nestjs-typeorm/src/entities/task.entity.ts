f6deab7e5436587551099e642ddcf16b74a81188
Hisatomo Banbara <banbara@S663.local>
2022-03-29 16:03:36 +0900

    Entityの作成

---
+++ nestjs-typeorm/src/entities/task.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    readonly task_id: number;
+
+    @Column({ type: 'varchar', length: 20, nullable: false })
+    title: string;
+
+    @Column({ type: 'date', nullable: false })
+    due_date: string;
+
+    @Column({ type: 'smallint', length: 1, default: 1 })
+    status: string;
+
+    @CreateDateColumn()
+    readonly created_at?: Date;
+
+    @UpdateDateColumn()
+    readonly update_at?: Date;
+
+}
